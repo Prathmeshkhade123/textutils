e67257278a225bf51ed38f6c20efdb9f516780b6
Prathmesh Khade <91484952+Prathmeshkhade123@users.noreply.github.com>
2023-02-03 11:27:25 +0530

    Updates

---
+++ precache-manifest.aa9f938d31dcf811622db861da28d426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27437b71a7f3f5e8c902",
+    "url": "/textutils/static/css/main.d8792cca.chunk.css"
+  },
+  {
+    "revision": "27437b71a7f3f5e8c902",
+    "url": "/textutils/static/js/main.27437b71.chunk.js"
+  },
+  {
+    "revision": "c31350e6c4d51234da9c",
+    "url": "/textutils/static/js/1.c31350e6.chunk.js"
+  },
+  {
+    "revision": "7cfeaf09478cd22b6f2e",
+    "url": "/textutils/static/js/2.7cfeaf09.chunk.js"
+  },
+  {
+    "revision": "7e94b02c436ae649e629",
+    "url": "/textutils/static/js/runtime~main.7e94b02c.js"
+  },
+  {
+    "revision": "33ea4b3efb62dcdcd22146351e690020",
+    "url": "/textutils/index.html"
+  }
+];
